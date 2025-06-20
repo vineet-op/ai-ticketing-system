7e7a028aab0dfb0c4a01f21e10390fe27749e66f
Vineet Jadhav <vineet290403@gmail.com>
2025-06-20 17:16:23 +0530

    Styles Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+
+import { Button } from './ui/button'
+import { useNavigate } from 'react-router-dom'
+import { LogOut, Ticket } from "lucide-react"
+
 
 const Navbar = () => {
+
+    const navigate = useNavigate()
+
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className='w-full pt-8 font-sans flex bg-neutral-950 items-center text-center justify-between px-10'>
+            <div className='text-lg font-normal flex gap-5 font-sans text-white text-center items-center justify-center'>
+                <Ticket className='text-green-500 text-xl size-7' />
+                <div className='text-xl'>
+                    Ticketify
+                </div>
+            </div>
+            <Button
+                onClick={() => {
+                    localStorage.removeItem('token');
+                    navigate("/login")
+                }}
+                className='bg-green-500 text-white text-center font-normal cursor-pointer hover:text-black'>
+                <LogOut /> Logout
+            </Button>
+        </nav>
     )
 }
 
